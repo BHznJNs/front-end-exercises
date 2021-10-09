4486e2774e21b5b5cb961008ebb72d1257bbe5c5
BHznJNs <441768875@qq.com>
2021-10-09 23:05:05 +0800

    commit

---
+++ page-slide-change-anime | 页面切换滑动动画/main.js
@@ -0,0 +1,10 @@
+const CONTAINER = document.querySelector("#container")
+let moved = 0
+
+function prePage() {
+    moved
+}
+function nextPage() {
+    moved -= 110
+    CONTAINER.getElementsByClassName.left = moved + "vw"
+}
