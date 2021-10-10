8f273070393677ee6c84af9620e604e1dc6fb4fc
BHznJNs <441768875@qq.com>
2021-10-10 13:30:16 +0800

    commit

--- page-slide-change-anime | 页面切换滑动动画/main.js
+++ page-slide-change-anime | 页面切换滑动动画/main.js
@@ -1,10 +1,36 @@
 const CONTAINER = document.querySelector("#container")
+const BTNs = document.querySelectorAll(".btn")
 let moved = 0
 
-function prePage() {
-    moved
+function sleep(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve()
+        }, time)
+    })
 }
-function nextPage() {
-    moved -= 110
-    CONTAINER.getElementsByClassName.left = moved + "vw"
+
+// 在动画进行中禁用按钮
+async function btnControler() {
+    for (let btn of BTNs) {
+        btn.style.pointerEvents="none"
+    }
+    await sleep(2000)
+    for (let btn of BTNs) {
+        btn.style.pointerEvents="auto"
+    }
+}
+
+// 动画控制函数
+async function pageChanger(toMove) {
+    btnControler()
+
+    CONTAINER.classList.add("changing")
+    await sleep(1000)
+
+    moved += toMove
+    CONTAINER.style.left = moved + "vw"
+
+    await sleep(1000)
+    CONTAINER.classList.remove("changing")
 }
